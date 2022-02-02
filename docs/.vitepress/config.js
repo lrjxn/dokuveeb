43ae16745f33cbbfc0931a83944bbbf494c1986a
lrjxn <laura.jaakson@tptlive.ee>
2022-02-02 12:27:58 +0200

    fondid

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,17 +1,31 @@
-const primarySidebar = [{text: "cvi", link:"/cvi/"}, {text:"briif", link:"/cvi/briif"}, {text:"moodboard", link:"/cvi/moodboard"}]
+const cviSidebar = [
+    {text: "cvi", link:"/cvi/"},
+    {text:"briif", link:"/cvi/briif"},
+    {text:"moodboard", link:"/cvi/moodboard"}]
+
+const logiSidebar = [
+    {text: "logi", link:"/logi/"},
+    {text:"Font märgiks", link:"/01_font"}]
+
+
 
 module.exports = {
     title: "Minu Dokuveeb",
     themeConfig: {
         nav: [
-            { text: "Home", link: "/" },
-            { text: "About", link: "/about/" },
-            { text: "Contact", link: "/contact/" },
+            { text: "Esileht", link: "/" },
+            { text: "Minust", link: "/about/" },
+            { text: "Logi", link: "/logi/" },
             { text: "CVI", link: "/cvi/" },
           ],
-          sidebar: {
-              "/cvi/":primarySidebar,
-              "/cvi/briif":primarySidebar,
-          } 
+          sidebar: 
+              {
+              "/cvi/":cviSidebar,
+              "/cvi/briif":cviSidebar,
+              "/cvi/moodboard":cviSidebar,
+              "/cvi/visandid":cviSidebar,
+            "/logi/":logiSidebar,
+            "/logi/01_font":logiSidebar,
+          },
     },
   };
---
+++ docs/logi/01_font.md
@@ -0,0 +1,7 @@
+# Tunniteema - fondi oma käe järgi muutmine
+Ülesande sisu - fondiga mängimine
+<img src="oma.nimi.margkis.png">
+
+## Ruutudest tähed
+Ülesande sisu - ruutudest korrektsete suhetega tähed.
+<img src="ruudud.tahed.png">
